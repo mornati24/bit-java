0e24d0825d9114fffbfefdbfd9c447ff8ed0adc4
mornati24 <kingdori2222@yonsei.ac.kr>
2020-08-06 12:39:33 +0900

    비트 연산, 3항연산 연습 연산자 마무리 콘솔 입출력 연습

--- javaEx/src/com/javaex/basic/OperEx.java
+++ javaEx/src/com/javaex/basic/OperEx.java
@@ -8,6 +8,8 @@ public class OperEx {
 		arithOper();
 		intcDecOper();
 		logicalOper();
+		bitOper();
+		conditionalOper();
 	}
 	// 산술연산자
 	public static void arithOper() {
@@ -76,8 +78,50 @@ public class OperEx {
 		b2 = n >= 10;
 		r = b1 || b2;
 		System.out.println("n<=0 or n>=10?" + r);
-		
+		// !(논리부정) : 논리값을 뒤집는다(true <-> false)
 		boolean notR = !r;
 		System.out.println("논리부정 : " + notR);
 	}
+	
+	//비트 연산자
+	public static void bitOper() {
+		// and(&), or(|), not(~)
+		// 정수형 데이터를 비트 단위로 제어하는 연산자
+		byte b1 = 0b1101;
+		byte b2 = 0b0111;
+		
+		int result = b1 & b2; // bit and연산
+		System.out.println(Integer.toBinaryString(result)); //0101
+		result = b1 | b2; // bit or연산
+		System.out.println(Integer.toBinaryString(result)); //1111
+		result = ~b1; // bit not연산 0<->1
+		System.out.println(Integer.toBinaryString(result)); //111...0010
+		System.out.println(result); // 맨앞의 1을 -로 인식해서 -14
+		//비트 시프트
+		// >> : 비트 왼쪽으로 이동(2배수 곱), << 비트 오른쪽으로 이동(2배수 나눗셈)
+		int val = 1;
+		System.out.println(val);
+		// 왼쪽으로 1비트 이동
+		System.out.println(Integer.toBinaryString(val<<1));
+		val = 0b100;
+		// 오른쪽으로 2비트 이동
+		System.out.println(Integer.toBinaryString(val>>2));
+	}
+	
+	//3항 연산자
+	public static void conditionalOper() {
+		//논리 비교 ? 참일때 : 거짓일때
+		// 간단한 값의 제어, 코드 가독성을 높일 수 있다.
+		int a = 10;
+		// a가 짝수면 "짝수", 아니면 "홀수" 문자열 반환
+		String message = a % 2 == 0 ? "짝수" : "홀수";
+		System.out.println("a는 짝수입니까? " + message);
+		int score = 60;
+		// 점수가 80 이상이면 Good
+		// 점수가 50 이상이면 Pass
+		// 그렇지 않으면 Fail
+		message = score >= 80 ? "Good" :  score >=50 ? "Pass" : "Fail"; //중첩이 가능하긴 하지만 가독성이 떨어지기 때문에 추천 X, 복잡한 비교는 비교문으로 해결
+		System.out.println("시험결과 : " + message);
+	}
+	
 }
