3e03f87f7cbfd6dac8b906b5d04bdc03493d68bb
mornati24 <kingdori2222@yonsei.ac.kr>
2020-08-10 12:45:04 +0900

    For 루프, continue와 break

--- javaEx/src/com/javaex/basic/ConditonalEx.java
+++ javaEx/src/com/javaex/basic/ConditonalEx.java
@@ -14,7 +14,10 @@ public class ConditonalEx {
 		//whileEx();
 		//whileEx2();
 		//whileEx3();
-		dowhileEx();
+		//dowhileEx();
+		//forEx();
+		//continueEx();
+		breakEx();
 	}
 	
 	public static void ifElseEx() {
@@ -211,6 +214,49 @@ public class ConditonalEx {
 		while(num != 0);
 			System.out.println("합산값 : " + total);
 			scanner.close();
+	}
+	public static void forEx() {
+		//문법 내에 제어변수 초기화, 반복 조건, 증감식을 모두 포함
+		//1. 반복 횟수가 정해져 있을 때
+		//2. 제어 변수를 for 문이 해주기 때문에 while보다 안전
+		
+		// 단수를 입력, 해당 단의 곱셈표를 출력(for문 버젼)
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("단을 입력 : ");
+		int dan = scanner.nextInt();
+		
+		for (int i = 1; i<=9; i++) {
+			// 내용 출력
+			System.out.println(dan + " * " + i + " = " + (dan*i));
+		}
+		
+		scanner.close();
+	}
+	public static void continueEx() {
+		//1부터 100까지 for loop
+		//2의 배수이거나 3의 배수면 출력 X
+		//나머지는 출력
+		for (int i = 1; i<=100; i++) {
+			//반복문 중간에 continue
+			//남은 구문은 실행하지 않고 다음번 루프 진행
+			if(i%2 == 0 || i %3 == 0) {
+				//아래 남은 코드는 실행 X, 다음 루프 진행
+				continue; // 예를 들어 i가 4면 출력 안됨.
+			}
+			System.out.println(i);
+		}
+	}
+	public static void breakEx() {
+		// 1부터 루프를 돌려서 6과 14로 모두 나누어 떨어지는 수는 얼마?
+		// 어디까지 루프를 돌려야 할 지 예측이 쉽지 않으니 while
+		int num = 1;
+		while(true) {
+			if (num % 6 == 0 && num % 14 == 0) {
+				break;
+			} 
+			num ++;
+		}
+		System.out.println(num);
 	}
 	
 }
