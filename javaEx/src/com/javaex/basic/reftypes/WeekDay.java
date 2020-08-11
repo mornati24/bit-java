afa90555d77610165b1525250c391bf1c2bd7530
mornati24 <kingdori2222@yonsei.ac.kr>
2020-08-11 12:49:47 +0900

    배열, 열거형

---
+++ javaEx/src/com/javaex/basic/reftypes/WeekDay.java
@@ -0,0 +1,16 @@
+package com.javaex.basic.reftypes;
+
+// 열거형
+// 값의 목록이 특정 값으로 제한되어 있을 때 활용
+// java의 경우 상수 , enum 많이 사용
+// enum 사용시 1. 코드가독성 올라감 2. 오류가능성 낮춤
+public enum WeekDay {
+	// 열거 상수 : 모두 대문자로
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+}
